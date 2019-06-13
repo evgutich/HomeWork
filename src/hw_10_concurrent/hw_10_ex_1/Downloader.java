be0f5ec6776058c8045666a4cb29c1ba74809dbf
evgutich <rozetko@yahoo.com>
2019-06-13 12:39:58 +0300

    concurrent hw ex1 fixed

--- src/hw_10_concurrent/hw_10_ex_1/Downloader.java
+++ src/hw_10_concurrent/hw_10_ex_1/Downloader.java
@@ -2,28 +2,25 @@ package hw_10_concurrent.hw_10_ex_1;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Arrays;
-import java.util.List;
 
 public class Downloader implements Runnable {
-    private static final List<String> FILE_URL = Arrays.asList("URL1", "URL2", "URL3", "URL4");
+    private final String url;
+
+    public Downloader(String url) {
+        this.url = url;
+    }
 
     @Override
     public void run() {
         downloadFile();
     }
 
-    private static synchronized void downloadFile() {
-        for (String url : FILE_URL) {
-            try (BufferedInputStream downl = new BufferedInputStream(new URL(url).openStream())) {
-                downl.read();
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+    private synchronized void downloadFile() {
+        try (BufferedInputStream downl = new BufferedInputStream(new URL(url).openStream())) {
+            downl.read();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
--- src/hw_10_concurrent/hw_10_ex_1/Test.java
+++ src/hw_10_concurrent/hw_10_ex_1/Test.java
@@ -1,11 +1,16 @@
 package hw_10_concurrent.hw_10_ex_1;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Test {
     public static void main(String[] args) {
-//        Thread first = new Thread(new Downloader());
-//        Thread second = new Thread(new Downloader());
-//        first.start();
-//        second.start();
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService.execute(new Downloader("url1"));
+        executorService.execute(new Downloader("url2"));
+        executorService.execute(new Downloader("url3"));
+        executorService.execute(new Downloader("url4"));
 
         Thread attributedThread = new Thread(new AttributedRunnable(),"Attributed Thread");
         attributedThread.setPriority(Thread.MIN_PRIORITY);
