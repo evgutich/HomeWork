f31c88f36c2de6ba2b62b37c3434cddb6ad0ffa4
evgutich <rozetko@yahoo.com>
2019-06-14 12:08:19 +0300

    java 8 hw fixed

--- src/hw_11_java_8/ListSorter.java
+++ src/hw_11_java_8/ListSorter.java
@@ -5,14 +5,14 @@ import java.util.stream.Stream;
 
 public class ListSorter {
     public static void main(String[] args) {
-        List<String> words = new ArrayList<>(Arrays.asList(("Wikipedia is a free online encyclopedia, created and edited by volunteers around the world and hosted by the Wikimedia Foundation\n" +
-                "DearneValleyOldMoorisan 89-hectare (220-acre) wetlands nature reserve in the .... Help desk – Ask questions about using Wikipedia").split("\\s+")));
+        List<String> words = new ArrayList<>(Arrays.asList("Wikipedia is a free online encyclopedia, created and edited by volunteers around the world and hosted by the Wikimedia Foundation\n" +
+                "89-hectare DearneValleyOldMoorisan (220-acre) wetlands nature reserve in the .... Help desk – Ask questions about using Wikipedia"));
         System.out.println(findMaxLongWord(words.stream()));
     }
 
     static Optional<String> findMaxLongWord(Stream<String> words){
         return words
-                .sorted(Comparator.comparing(word -> -word.split(" ")[0].length()))
-                .findFirst();
+                .flatMap(word -> Arrays.asList(word.split("\\s+")).stream())
+                .max(Comparator.comparing(word -> word.length()));
     }
 }
